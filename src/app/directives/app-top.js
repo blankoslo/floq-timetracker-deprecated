a61363b3fe729b27c07f26f020a53c0f602c4bea
Lasse Jul-Larsen <lassejl@gmail.com>
2016-04-11 16:08:54 +0200

    Fixed app header abit

--- src/app/directives/app-top.js
+++ src/app/directives/app-top.js
@@ -1,11 +1,17 @@
+import moment from 'moment';
+
 export default () => {
   return {
     template: require('../views/app-top.html'),
     controller: ($scope, Auth) => {
+
       $scope.bg = Auth.getTopImage();
       $scope.$on('userChanged', (event, user) => {
         $scope.employee = user;
       })
+      $scope.$on('dateChanged', (event, date) => {
+        $scope.month = moment(date).format("MMMM YYYY");
+      });
     }
   }
 }
