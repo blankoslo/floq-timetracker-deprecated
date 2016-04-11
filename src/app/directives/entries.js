a8a9319807d800f3721f5f43a8b18d916f8ff357
Lasse Jul-Larsen <lassejl@gmail.com>
2016-04-11 12:17:04 +0200

    Fixed optimistic updating of time entries. Changed position and size of dialog to "hack" position fixed issues.

--- src/app/directives/entries.js
+++ src/app/directives/entries.js
@@ -1,61 +1,74 @@
 import moment from 'moment';
 
 export default function() {
+    var MINUTE_INCREMENT = 30;
+    var MINUTE_INCREMENT_FIRST_CLICK = 450;
 
-  var controller = ($scope, Api, Auth) => {
-    var selectedDay = moment().format("YYYY-MM-DD");
+    var controller = ($scope, $rootScope, Api, Auth) => {
+        var selectedDay = moment().format("YYYY-MM-DD");
 
-    $scope.logHour = (entry) => {
-      Api.logEntry(entry.customer, entry.project, Auth.getEmployee().id, selectedDay, 30).then(() => {
-        updateEntries();
-      });
-    };
+        $scope.logHour = (entry) => {
+            var logMinutes = 0;
+            if (entry.time == 0) {
+                entry.time = MINUTE_INCREMENT_FIRST_CLICK;
+                logMinutes = MINUTE_INCREMENT_FIRST_CLICK;
+            } else {
+                logMinutes = MINUTE_INCREMENT;
+                entry.time += MINUTE_INCREMENT;
+            }
+            $rootScope.$broadcast('entryChanged', logMinutes, selectedDay);
+            Api.logEntry(entry.customer, entry.project, Auth.getEmployee().id, selectedDay, logMinutes).then(() => {
+                //updateEntries();
+            });
+        };
 
-    $scope.removeHour = (entry) => {
-      Api.logEntry(entry.customer, entry.project, Auth.getEmployee().id, selectedDay, -30).then(() => {
-        updateEntries();
-      });
-    }
+        $scope.removeHour = (entry) => {
+            entry.time -= MINUTE_INCREMENT;
+            $rootScope.$broadcast('entryChanged', -MINUTE_INCREMENT, selectedDay);
+            Api.logEntry(entry.customer, entry.project, Auth.getEmployee().id, selectedDay, -MINUTE_INCREMENT).then(() => {
+                //updateEntries();
+            });
+        }
 
-    $scope.$on('dateChanged', (event, date) => {
-      selectedDay = date;
-      updateEntries();
-    })
-
-    $scope.$on('projectAppended', (event, project) => {
-      var id = project.customer+ project.code;
-      var exists = false;
-      $scope.entries.forEach((entry) => {
-        if (entry.id == id) exists = true;
-      });
-
-      if (!exists) {
-        $scope.entries.push({
-          customer: project.customer,
-          project: project.code,
-          title: project.title,
-          time: 0
+        $scope.$on('dateChanged', (event, date) => {
+            selectedDay = date;
+            updateEntries();
         })
-      } else {
-        console.log("Project already exists");
-      }
-    })
 
-    function updateEntries() {
-      if (Auth.getEmployee().id) {
-        Api.getEntries(Auth.getEmployee().id, selectedDay).then((response) => {
-          $scope.entries = response.data;
-        });
-      }
+        $scope.$on('projectAppended', (event, project) => {
+            var id = project.customer + project.code;
+            var exists = false;
+            $scope.entries.forEach((entry) => {
+                if (entry.id == id) exists = true;
+            });
+
+            if (!exists) {
+                $scope.entries.push({
+                    customer: project.customer,
+                    project: project.code,
+                    title: project.title,
+                    time: 0
+                })
+            } else {
+                console.log("Project already exists");
+            }
+        })
+
+        function updateEntries() {
+            if (Auth.getEmployee().id) {
+                Api.getEntries(Auth.getEmployee().id, selectedDay).then((response) => {
+                    $scope.entries = response.data;
+                });
+            }
+        }
+
+        $scope.$on('userChanged', (user) => {
+            updateEntries();
+        })
     }
 
-    $scope.$on('userChanged', (user) => {
-      updateEntries();
-    })
-  }
-
-  return {
-    template: require('../views/entries.html'),
-    controller: controller
-  }
+    return {
+        template: require('../views/entries.html'),
+        controller: controller
+    }
 }
--- src/app/directives/weeklyCalendar.js
+++ src/app/directives/weeklyCalendar.js
@@ -25,19 +25,19 @@ export default function() {
       $scope.week = buildWeek(weekStart);
 
       $scope.getHeader = () => {
-          var weekNumber = weekStart.get('week');
-          var startDate = weekStart.get('date');
-          var endOfWeek = weekStart.clone().endOf("isoweek");
-          var endDate = endOfWeek.get('date');
-          var year = endOfWeek.format('YYYY');
-          var month = () => {
-            if (weekStart.isSame(endOfWeek, 'month')) {
-              return weekStart.format('MMMM')
-            } else {
-              return `${weekStart.format('MMMM')}/${endOfWeek.format('MMMM')}`
-            }
+        var weekNumber = weekStart.get('week');
+        var startDate = weekStart.get('date');
+        var endOfWeek = weekStart.clone().endOf("isoweek");
+        var endDate = endOfWeek.get('date');
+        var year = endOfWeek.format('YYYY');
+        var month = () => {
+          if (weekStart.isSame(endOfWeek, 'month')) {
+            return weekStart.format('MMMM')
+          } else {
+            return `${weekStart.format('MMMM')}/${endOfWeek.format('MMMM')}`
           }
-          return `Uke ${weekNumber} ${startDate} - ${endDate}. ${month()} ${year}`;
+        }
+        return `Uke ${weekNumber} ${startDate} - ${endDate}. ${month()} ${year}`;
       }
 
       $scope.select = (date) => {
@@ -45,41 +45,52 @@ export default function() {
       }
 
       $scope.isSelected = (date) => {
-          return moment(date).isSame($scope.selected, 'day');
+        return moment(date).isSame($scope.selected, 'day');
       }
 
       $scope.previous = () => {
-          weekStart = weekStart.clone().subtract(1, 'w');
-          $scope.week = buildWeek(weekStart);
-          $scope.selected = weekStart;
-          $scope.weekNumber = weekStart.get('week');
-          fetchHoursForWeek();
+        weekStart = weekStart.clone().subtract(1, 'w');
+        $scope.week = buildWeek(weekStart);
+        $scope.selected = weekStart;
+        $scope.weekNumber = weekStart.get('week');
+        fetchHoursForWeek();
       };
 
       $scope.next = () => {
-          weekStart = weekStart.clone().add(1, 'w');
-          $scope.week = buildWeek(weekStart);
-          $scope.selected = weekStart;
-          $scope.weekNumber = weekStart.get('week');
-          fetchHoursForWeek();
+        weekStart = weekStart.clone().add(1, 'w');
+        $scope.week = buildWeek(weekStart);
+        $scope.selected = weekStart;
+        $scope.weekNumber = weekStart.get('week');
+        fetchHoursForWeek();
       }
 
       function fetchHoursForWeek() {
         Api.getWeeklyEntries(Auth.getEmployee().id, weekStart.format('YYYY-MM-DD')).then((result) => {
-          appendHours($scope.week, result.data);
+          appendWeeklyLog(result.data);
         });
       }
 
-      function appendHours(week, hours) {
+      function appendWeeklyLog(hours) {
         hours.forEach((e) => {
-          var day = moment(e.date).get('day');
-          week[day-1].logged = e.sum;
+          appendTime(e.date, e.sum);
+        });
+      }
+
+      function appendTime(date, time) {
+        $scope.week.forEach((day) => {
+          if (moment(date).isSame(day.date, 'day')) {
+            day.logged = parseInt(day.logged) + time
+          }
         });
       }
 
       $scope.$on('userChanged', () => {
         fetchHoursForWeek();
-      })
+      });
+
+      $scope.$on('entryChanged', (event, minutes, day) => {
+        appendTime(day, minutes);
+      });
 
       $scope.$watch('selected', (change) => {
         $rootScope.$broadcast('dateChanged', $scope.selected.format('YYYY-MM-DD'));
