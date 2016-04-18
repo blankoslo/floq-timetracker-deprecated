715f6d7988bbb40afe2df1aa98ef78beec2b878f
Erlend Hamberg <eh@blankoslo.no>
2016-04-18 10:44:23 +0200

    Handle `projects` being `null`

--- src/app/directives/entries.js
+++ src/app/directives/entries.js
@@ -10,9 +10,12 @@ export default () => {
     function fetchEntries() {
       if (Auth.getEmployee().id) {
         Api.getEntries(Auth.getEmployee().id, selectedDay).then((response) => {
-          $scope.entries = response.data[0].projects.map(p =>
-              // synthesize some properties for internal bookkeeping
-              Object.assign({ logged: p.minutes, hours: p.minutes / 60 }, p));
+          const projects = response.data[0].projects;
+          $scope.entries = projects
+                         ? response.data[0].projects.map(p =>
+                             // synthesize some properties for internal bookkeeping
+                             Object.assign({ logged: p.minutes, hours: p.minutes / 60 }, p))
+                         : [];
         });
       }
     }
