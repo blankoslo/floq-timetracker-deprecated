cde76803b4237e2b3970bea3e7b62f10e7ffe81d
John Andre Hestad <jah@blankoslo.no>
2016-10-06 11:11:17 +0200

    Connect locked date in controller with api service

--- src/app/controllers/calendarCtrl.js
+++ src/app/controllers/calendarCtrl.js
@@ -50,7 +50,7 @@ export default class CalendarCtrl {
     }
 
     $scope.selected = moment();
-    $scope.locked = null;
+    $scope.locked = moment(Api.getLockedDate(Auth.getEmployee().id));
     $scope.week = buildWeek(weekStart);
 
     function fetchHolidaysForWeek() {
@@ -108,6 +108,7 @@ export default class CalendarCtrl {
     $scope.isSelected = date => moment(date).isSame($scope.selected, 'day');
 
     $scope.lock = (date) => {
+      Api.logLockedDate(Auth.getEmployee().id, date);
       $scope.locked = moment(date);
     };
 
